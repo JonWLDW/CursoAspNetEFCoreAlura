c477c0e3caf3ba2b70b654fe6fab877d40f39ddd
Jonatan Waldow <jonwldw@gmail.com>
2020-05-31 13:33:07 -0400

    Entendimento sobre tipos de injeção de dependência e ciclo de vida dos objetos.

--- cursoAspNetEfCoreAlura/cursoAspNetEfCoreAlura/Startup.cs
+++ cursoAspNetEfCoreAlura/cursoAspNetEfCoreAlura/Startup.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,10 +12,12 @@ namespace cursoAspNetEfCoreAlura
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddScoped<ICatalogoDeLivros, CatalogoDeLivros>();
+			services.AddScoped<IRelatorioDeLivros, RelatorioDeLivros>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
 		{
 			if (env.IsDevelopment())
 			{
@@ -27,9 +25,9 @@ namespace cursoAspNetEfCoreAlura
 			}
 
 			app.UseRouting();
-			
-			var catalogo = new CatalogoDeLivros();
-			var relatorio = new RelatorioDeLivros(catalogo);
+
+			//var catalogo = serviceProvider.GetService<ICatalogoDeLivros>();
+			var relatorio = serviceProvider.GetService<IRelatorioDeLivros>();
 
 			app.UseEndpoints(endpoints =>
 			{
