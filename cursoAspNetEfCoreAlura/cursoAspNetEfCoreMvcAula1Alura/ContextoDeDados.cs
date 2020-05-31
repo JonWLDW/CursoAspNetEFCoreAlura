2c110d9e8fbc45ec368b295455a317bd82d6f7a4
Jonatan Waldow <jonwldw@gmail.com>
2020-05-31 14:36:42 -0400

    Realizada configuração do EntityFramework Core e criado o primeiro migration.

---
+++ cursoAspNetEfCoreAlura/cursoAspNetEfCoreMvcAula1Alura/ContextoDeDados.cs
@@ -0,0 +1,19 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo
+{
+	public class ContextoDeDados : DbContext
+	{
+		public ContextoDeDados(DbContextOptions options) : base(options)
+		{
+			
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Produto>().HasKey(t => t.Id);
+		}
+	}
+}
--- cursoAspNetEfCoreAlura/cursoAspNetEfCoreMvcAula1Alura/Startup.cs
+++ cursoAspNetEfCoreAlura/cursoAspNetEfCoreMvcAula1Alura/Startup.cs
@@ -4,47 +4,53 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CasaDoCodigo
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseBrowserLink();
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+			var connectionStrings = Configuration.GetConnectionString("Default");
+			services.AddDbContext<ContextoDeDados>(options =>
+				options.UseSqlServer(connectionStrings)
+			);
+		}
 
-            app.UseStaticFiles();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
-            });
-        }
-    }
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Pedido}/{action=Carrossel}/{id?}");
+			});
+		}
+	}
 }
